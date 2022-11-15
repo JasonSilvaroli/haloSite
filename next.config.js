6c96dba5cb7f18c22e9cf18a9cf34754758f6258
Jason Silvaroli <jsilvaroli@myseneca.ca>
2022-11-15 18:26:58 -0500

    added tab favicon and name

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "https://assets.halo.autocode.gg"
+      },
+      {
+        protocol: "https",
         hostname: "halostats.ca"
       }
     ]
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import createEmotionCache from "../utility/createEmotionCache";
 import lightThemeOptions from "../styles/theme/lightThemeOptions";
 import "../styles/globals.css";
 import { NavBar } from "../components/navBar";
+import Head from "next/head";
 interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
 }
@@ -30,6 +31,27 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
 
     return (
         <CacheProvider value={emotionCache}>
+            <Head>
+                <title>Halo Stats</title>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             <ThemeProvider theme={lightTheme}>
                 <NavBar></NavBar>
                 <CssBaseline />
